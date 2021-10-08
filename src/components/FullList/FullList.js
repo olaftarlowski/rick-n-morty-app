01e46c74c976605327f5493858773e03ec02498b
olaftarlowski <olaf.tarlowski@gmail.com>
2021-10-08 11:22:35 +0200

    data fetch error handling added

--- src/components/FullList/FullList.js
+++ src/components/FullList/FullList.js
@@ -1,51 +1,62 @@
 import { useEffect, useState } from "react";
-import ListItem from '../ListItem/ListItem';
+import ListItem from "../ListItem/ListItem";
 
 const FullList = () => {
-    const [dataItems, setDataItems] = useState([]);
+  const [dataItems, setDataItems] = useState([]);
+  const [error, setError] = useState(null);
 
-    const [pagesToLoad, setPagesToLoad] = useState(5);
-    const [bottomCounterIndicator, setBottomCounterIndicator] = useState(1);
+  const [pagesToLoad, setPagesToLoad] = useState(5);
+  const [bottomCounterIndicator, setBottomCounterIndicator] = useState(1);
 
-    useEffect(() => {
-        const getCharactersData = async () => {
-            const apiURL = "https://rickandmortyapi.com/api/character/";
-            let apiData = [];
+  useEffect(() => {
+    const getCharactersData = async () => {
+      const apiURL = "https://rickandmortyapi.com/api/character/";
+      let apiData = [];
 
-            for (let i = bottomCounterIndicator; i <= pagesToLoad && i <= 34; i++) {
-                const response = await fetch(apiURL + "?page=" + i);
-                const data = await response.json();
-                apiData = apiData.concat(data.results);
-            }
-
-            setDataItems((element) => element.concat(apiData));
+      try {
+        for (let i = bottomCounterIndicator; i <= pagesToLoad && i <= 34; i++) {
+          const response = await fetch(apiURL + "?page=" + i);
+          if (!response.ok) {
+            throw new Error("Something went wrong!");
+          }
+          const data = await response.json();
+          apiData = apiData.concat(data.results);
         }
-        getCharactersData();
-    }, [pagesToLoad, bottomCounterIndicator])
+      } catch (error) {
+        setError(error.message);
+      }
 
-    const loadMorePagesHandler = () => {
-        setBottomCounterIndicator(pagesToLoad + 1);
-        setPagesToLoad((state) => {
-            return state + 5;
-        });
+      setDataItems((element) => element.concat(apiData));
     };
+    getCharactersData();
+  }, [pagesToLoad, bottomCounterIndicator]);
 
-    const loadAllDataHandler = () => {
-        setBottomCounterIndicator(pagesToLoad + 1);
-        setPagesToLoad(34);
-    };
+  const loadMorePagesHandler = () => {
+    setBottomCounterIndicator(pagesToLoad + 1);
+    setPagesToLoad((state) => {
+      return state + 5;
+    });
+  };
 
-    return (
-        <div>
-            <ul>
-                {dataItems.map((element) => {
-                    return <ListItem key={element.id} id={element.id} name ={element.name}/>
-                })}
-            </ul>
-            <button onClick={loadMorePagesHandler}>Show more results (100)</button>
-            <button onClick={loadAllDataHandler}>Show all results</button>
-        </div>
-    )
-}
+  const loadAllDataHandler = () => {
+    setBottomCounterIndicator(pagesToLoad + 1);
+    setPagesToLoad(34);
+  };
+
+  return (
+    <div>
+      <ul>
+        {error ? `${error}` : ""}
+        {dataItems.map((element) => {
+          return (
+            <ListItem key={element.id} id={element.id} name={element.name} />
+          );
+        })}
+      </ul>
+      <button onClick={loadMorePagesHandler}>Show more results (100)</button>
+      <button onClick={loadAllDataHandler}>Show all results</button>
+    </div>
+  );
+};
 
 export default FullList;
